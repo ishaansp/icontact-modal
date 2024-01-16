0c4c2558a690d25b35b0afc2c815d825bd6d91b2
ishaansp <ishaan_sharma@softprodigy.com>
2024-01-16 11:43:15 +0530

    initial commit

---
+++ pages/test.vue
@@ -0,0 +1,92 @@
+<template>
+  <div>
+    <nuxt-link to="/"> Home </nuxt-link>
+    <button @click="openModal" id="myBtn">Open Modal</button>
+
+    <!-- The Modal -->
+    <div id="myModal" class="modal">
+      <!-- Modal content -->
+      <div class="modal-content">
+        <span @click="closeModal" class="close">&times;</span>
+        <!-- <p>Some text in the Modal..</p> -->
+        <div id="icontactSignupFormWrapper473"><script type="text/javascript" async src="https://app.icontact.com/icp/core/mycontacts/signup/designer/form/automatic?id=473&cid=1912706&lid=7659&divid=icontactSignupFormWrapper473"></script></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      showModal: false,
+    };
+  },
+  methods: {
+    openModal() {
+
+      // this.showModal = true;
+var modal = document.getElementById("myModal");
+
+      modal.style.display = "block";
+    //   const script = document.createElement('script')
+	// 	script.type = 'text/javascript'
+	// 	script.src = "https://app.icontact.com/icp/core/mycontacts/signup/designer/form/automatic?id=473&cid=1912706&lid=7659&divid=icontactSignupFormWrapper473"
+	// 	script.async = true
+	// 	script.defer = true
+
+	// 	this.scriptele = document.body.appendChild(script)
+
+
+
+    },
+    closeModal() {
+      // this.showModal = false;
+// var span = document.getElementsByClassName("close")[0];
+var modal = document.getElementById("myModal");
+modal.style.display = "none";
+
+
+    },
+  },
+};
+</script>
+<style scoped>
+/* The Modal (background) */
+.modal {
+  display: none; /* Hidden by default */
+  position: fixed; /* Stay in place */
+  z-index: 1; /* Sit on top */
+  left: 0;
+  top: 0;
+  width: 100%; /* Full width */
+  height: 100%; /* Full height */
+  overflow: auto; /* Enable scroll if needed */
+  background-color: rgb(0, 0, 0); /* Fallback color */
+  background-color: rgba(0, 0, 0, 0.4); /* Black w/ opacity */
+}
+
+/* Modal Content/Box */
+.modal-content {
+  background-color: #fefefe;
+  margin: 15% auto; /* 15% from the top and centered */
+  padding: 20px;
+  border: 1px solid #888;
+  width: 80%; /* Could be more or less, depending on screen size */
+}
+
+/* The Close Button */
+.close {
+  color: #aaa;
+  float: right;
+  font-size: 28px;
+  font-weight: bold;
+}
+
+.close:hover,
+.close:focus {
+  color: black;
+  text-decoration: none;
+  cursor: pointer;
+}
+</style>
